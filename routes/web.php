cc2e3b858076ec1dbd9472598cab7971b7f4aec4
trjgdyan <trjgdyan@gmail.com>
2024-07-16 11:23:47 +0700

    First commit, template blade, login, register

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/app', function () {
+    return view('layouts.app');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
